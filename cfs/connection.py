73e888d94bc64b47a2fd36020c6b5f4497e9239d
Guilherme Santos <guilherme-ads@live.com>
2020-06-05 21:13:29 -0300

    Fixing MYSQL connection

--- cfs/connection.py
+++ cfs/connection.py
@@ -11,6 +11,7 @@ class Start:
             bd = self.mysql.connect(
                 host="localhost",
                 user="root",
+                passwd="admin",
                 db="industry"
             )
             return bd
