7c422d8491550183b28f2d37ee454c0100fb0422
Guilherme Santos <guilherme-ads@live.com>
2020-06-07 23:42:16 -0300

    Updating files provided

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@
 WORKDIR="/home/peanut/dotz"
 
 echo "Perfomando operações necessárias "
-pip install -r requirements.txt
+
+python3 -m pip install -r requirements.txt
 
 sudo /etc/init.d/mysql start
 
